ecb42fe538df986d2648c58ce2557c5054fdb770
Sarsen <blazer.fei@gmail.com>
2024-05-18 14:55:43 +0500

    Adaptive Slike Slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-	$('.carousel__inner').slick();
+	$('.carousel__inner').slick({
+		speed: 1300,
+		slidesToShow: 1,
+		adaptiveHeight: true,
+		prevArrow:'<button type="button" class="slick-prev"><img src="../images/chevron left solid.png"></button>',
+		nextArrow:'<button type="button" class="slick-next"><img src="../images/chevron right solid.png"></button>',
+		responsive: [
+			{
+				breakpoint: 880,
+				settings: {
+				  arrows:false,
+				  dots: true,
+				  variableWidth:true
+
+				}
+			}
+		]
+	  });
   });
