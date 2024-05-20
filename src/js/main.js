41dabd134f05a47cf2f8c3fdde9240195f578833
Sarsen <blazer.fei@gmail.com>
2024-05-20 16:30:36 +0500

    Modal

--- src/js/main.js
+++ src/js/main.js
@@ -39,4 +39,23 @@ $(document).ready(function(){
 	  toggleClass('.catalog-item__link');
 	  toggleClass('.catalog-item__back');
 
+
+//modal  data-modal="consultation">
+
+$('[data-modal="consultation"]').on('click', function () {
+$('.overlay, #consultation').fadeIn('slow')
+});
+
+$('.modal__close').on('click',function() {
+	$('.overlay, #consultation ,#order, #thanks').fadeOut('slow')
+});
+
+
+$('.button__mini').each(function (i) {
+ $(this).on('click',function () {
+	$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+	$('.overlay, #order').fadeIn('slow')
+ })
+});
+
   });
