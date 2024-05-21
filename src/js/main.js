c7086b275d48ed6feb2b4e05fa184223cbd95c5b
Sarsen <blazer.fei@gmail.com>
2024-05-21 15:00:44 +0500

    Smooth Scroll

--- src/js/main.js
+++ src/js/main.js
@@ -115,6 +115,22 @@ $('form').submit(function(e) {
 	return false
 });
 
+//smooth scroll and pageup
+$(window).scroll(function() {
+if($(this).scrollTop() > 1600){
+	$('.pageup').fadeIn();
+} else {
+	$('.pageup').fadeOut();
+}
+});
+
+
+$("a[href^='#']").click(function() {
+	let _href = $(this).attr("href");
+	$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+	return false
+})
+
 
 });
  
