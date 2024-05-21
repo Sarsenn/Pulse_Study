ba6fa936207a1389a74184130a539f2ebd4a8bd7
Sarsen <blazer.fei@gmail.com>
2024-05-21 15:59:20 +0500

    Animation and WoW

--- src/js/main.js
+++ src/js/main.js
@@ -129,7 +129,10 @@ $("a[href^='#']").click(function() {
 	let _href = $(this).attr("href");
 	$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
 	return false
-})
+});
+
+
+new WOW().init();
 
 
 });
