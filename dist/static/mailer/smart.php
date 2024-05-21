9bd2f097030cceba65392eda33927aae2d93233c
Sarsen <blazer.fei@gmail.com>
2024-05-21 14:06:19 +0500

    Mailer

--- dist/static/mailer/smart.php
+++ dist/static/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Username = 'blazer.fei@gmail.com';                 // Наш логин
+$mail->Password = 'yxil eqcx fsni xmmi';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('', 'Pulse');   // От кого письмо 
-$mail->addAddress('');     // Add a recipient
+$mail->setFrom('blazer.fei@gmail.com ', 'Pulse');   // От кого письмо 
+$mail->addAddress('janec72847@huleos.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
